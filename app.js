298924c5425c7d66b71f6367cb4ac3901df57647
Tshepo Ntokoane <Tshepo@Pauls-iMac.local>
2021-03-19 11:25:49 +0200

    env

--- app.js
+++ app.js
@@ -5,6 +5,7 @@ app.get('/', (req, res) => {
     res.send("<html><head><title>Bitcube Interview</title></head><body bgcolor='#c0c0c0'><h3> This is my Bitcube interview</h3></body></html>");
 });
 
-app.listen(8080, () => {
+const port = process.env.port || 8080;
+app.listen(port, () => {
     console.log("We are Live")
 });
